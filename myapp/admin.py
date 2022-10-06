9abdbad9e7f815fee59b888d8392af0b6b7ef2a4
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2022-10-06 16:55:21 -0300

    ✨ Add models to myapp
    Co-authored-by: Matheus Rezende <Matheus3007@users.noreply.github.com>

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from .models import Voo, CaracteristicasReais
+
+admin.site.register(Voo)
+admin.site.register(CaracteristicasReais)
--- myapp/models.py
+++ myapp/models.py
@@ -1,1 +1,43 @@
 from django.db import models
+
+class Voo(models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    companhia_aerea = models.CharField(max_length=25)
+    origem = models.CharField(max_length=25)
+    destino = models.CharField(max_length=25)
+    partida_prevista = models.TimeField()
+    chegada_prevista = models.TimeField()
+
+    class Meta:
+        verbose_name = "Voo"
+        verbose_name_plural = "Voos"
+
+    def __str__(self):
+        return self.codigo
+
+class CaracteristicasReais(models.Model):
+    status_choices = (
+        (0, ""),
+        (-1, "Cancelado"),
+        (1, "Embarcando"),
+        (2, "Programado"),
+        (3, "Taxiando"),
+        (4, "Pronto"),
+        (5, "Autorizado"),
+        (6, "Em voo"),
+        (7, "Aterrissado"),
+    )
+
+    voo = models.ForeignKey(Voo, on_delete=models.CASCADE)
+    dia = models.DateField()
+    status = models.IntegerField(choices=status_choices, default=0)
+    partida_real = models.TimeField(null=True, blank=True)
+    chegada_real = models.TimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Caracteristicas Reais"
+        verbose_name_plural = "Caracteristicas Reais"
+        constraints = [models.UniqueConstraint(fields=["voo", "dia"], name="unique_voo_dia")]
+
+    def __str__(self):
+        return self.voo.codigo + " - " + self.dia.strftime("%d/%m/%Y")
