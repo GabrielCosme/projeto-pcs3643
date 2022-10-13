7929a256a7292c58f3cc6399620f950965125ac6
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2022-10-13 17:18:14 -0300

    💄 Add login page

--- MyProj/settings.py
+++ MyProj/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
@@ -123,3 +123,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = '/admin'
