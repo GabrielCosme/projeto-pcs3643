7df207e5e84313b379e50bf77b96e48c421fbc6f
Matheus Rezende <matheusrp2009@hotmail.com>
2022-11-06 23:57:42 -0300

    Adds form tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -39,12 +39,42 @@ class test_view_area_do_funcionario(TestCase):
         group.user_set.add(user)
         group.save()
         user.save()
+        cls.voo = Voo.objects.create(codigo="AA123", companhia_aerea = "ABC", origem = "SP", destino = "MG", partida_prevista = "10:00", chegada_prevista = "11:00")
+        cls.voo.save()
 
+    def test_model_content(self):
+        self.client.login(username="funcionario", password="bola1234")
+        self.assertEqual(self.voo.codigo, "AA123")
+        self.assertEqual(self.voo.companhia_aerea, "ABC")
+        self.assertEqual(self.voo.origem, "SP")
+        self.assertEqual(self.voo.destino, "MG")
+        self.assertEqual(self.voo.partida_prevista, "10:00")
+        self.assertEqual(self.voo.chegada_prevista, "11:00")
+
+
+    
     def test_area_do_funcionario(self):
         self.client.login(username="funcionario", password="bola1234")
         response = self.client.get("/areaDoFuncionario/", follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "areaDoFuncionario.html")
+
+    def test_funcionario_cria_voo_real(self):
+        self.client.login(username="funcionario", password="bola1234")
+        self.client.post("/areaDoFuncionario/", {"operation": "create", "codigo": "AA123", "dia": "2020-01-01", "status": 0}, follow=True)
+        self.assertEqual(VooReal.objects.first().status, 0)
+
+    def test_funcionario_altera_voo_real(self):
+        self.client.login(username="funcionario", password="bola1234")
+        VooReal.objects.create(voo=self.voo, dia="2020-01-01", status=0)
+        self.client.post("/areaDoFuncionario/", {"operation": "update", "codigo": "AA123", "dia": "2020-01-01", "status": 1}, follow=True)
+        self.assertEqual(VooReal.objects.first().status, 1)
+
+    def test_funcionario_altera_ordem_invalida(self):
+        self.client.login(username="funcionario", password="bola1234")
+        VooReal.objects.create(voo=self.voo, dia="2020-01-01", status=0)
+        self.client.post("/areaDoFuncionario/", {"operation": "update", "codigo": "AA123", "dia": "2020-01-01", "status": 2}, follow=True)
+        self.assertEqual(VooReal.objects.first().status, 0)
 
     def test_cadastrar_voo_real(self):
         self.client.login(username="funcionario", password="bola1234")
@@ -96,12 +126,29 @@ class test_view_area_do_operador(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "consultarVoo.html")
 
-    def test_editar_voo(self):
+    def test_view_editar_voo(self):
         self.client.login(username="operador", password="bola1234")
         response = self.client.get("/editarVoo/?codigo=ABC1342", follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "editarVoo.html")
 
+    def test_cadastrar_voo(self):
+        self.client.login(username="operador", password="bola1234")
+        self.client.post("/areaDoOperador/", {"operation": "create", "companhia_aerea": "LAT", "codigo": "123", "origem": "sp", "destino": "mg", "horario_partida_prevista": "00:10", "horario_chegada_prevista": "00:30"}, follow=True)
+        self.assertEqual(Voo.objects.first().codigo, "123")
+
+    def test_editar_voo(self):
+        self.client.login(username="operador", password="bola1234")
+        Voo.objects.create(codigo="123", companhia_aerea = "123", origem = "sp", destino = "mg", partida_prevista = "00:10", chegada_prevista = "00:30")
+        self.client.post("/areaDoOperador/", { "codigo": "123", "operation": "update", "companhia_aerea": "TAL",  "origem": "sp", "destino": "mg", "horario_partida_prevista": "00:10", "horario_chegada_prevista": "00:30"}, follow=True)
+        self.assertEqual(Voo.objects.first().companhia_aerea, "TAL")
+
+    def test_deletar_voo(self):
+        self.client.login(username="operador", password="bola1234")
+        Voo.objects.create(codigo="123", companhia_aerea = "123", origem = "sp", destino = "mg", partida_prevista = "00:10", chegada_prevista = "00:30")
+        itemCount = Voo.objects.count()
+        self.client.post("/areaDoOperador/", { "codigo": "123", "operation": "delete"}, follow=True)
+        self.assertEqual(Voo.objects.count(), itemCount - 1)
 
 class test_view_area_do_gerente(TestCase):
     @classmethod
@@ -122,6 +169,20 @@ class test_view_area_do_gerente(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "areaDoGerente.html")
 
+    def test_gera_relatorio_data(self):
+        self.client.login(username="gerente", password="bola1234")
+        response = self.client.post("/areaDoGerente/", {"data_inicio":"2022-11-1", "data_fim":"2022-11-1", "companhia_aerea":""}, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_gera_relatorio_cia_aerea(self):
+        self.client.login(username="gerente", password="bola1234")
+        response = self.client.post("/areaDoGerente/", {"data_inicio":"", "data_fim":"", "companhia_aerea":"GOL"}, follow=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_gera_relatorio_misto(self):
+        self.client.login(username="gerente", password="bola1234")
+        response = self.client.post("/areaDoGerente/", {"data_inicio":"2022-11-1", "data_fim":"2022-11-1", "companhia_aerea":"GOL"}, follow=True)
+        self.assertEqual(response.status_code, 200)
 
 class VooTestCase(TestCase):
     @classmethod
